96e43c4eb175916764801239f3201d7a32b280bf
tonymushah <tonyramanantsalama@gmail.com>
2024-01-10 11:30:43 +0300

    Added dotenv

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("me.paulschwarz:spring-dotenv:4.0.0")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
