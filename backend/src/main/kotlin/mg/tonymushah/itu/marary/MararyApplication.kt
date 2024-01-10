0974151a5b21edd20c2d46c2c756bf79f962a293
tonymushah <tonyramanantsalama@gmail.com>
2024-01-10 11:17:58 +0300

    Init Backend

---
+++ backend/src/main/kotlin/mg/tonymushah/itu/marary/MararyApplication.kt
@@ -0,0 +1,11 @@
+package mg.tonymushah.itu.marary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MararyApplication
+
+fun main(args: Array<String>) {
+	runApplication<MararyApplication>(*args)
+}
---
+++ backend/src/test/kotlin/mg/tonymushah/itu/marary/MararyApplicationTests.kt
@@ -0,0 +1,13 @@
+package mg.tonymushah.itu.marary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MararyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
