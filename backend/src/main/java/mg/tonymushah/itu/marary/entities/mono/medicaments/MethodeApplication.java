deddecd29bc3ad8ea80b345380f9a5f513724870
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 19:57:07 +0300

    Added `method_appl_medicament_id_seq`

--- backend/src/main/java/mg/tonymushah/itu/marary/entities/mono/medicaments/MethodeApplication.java
+++ backend/src/main/java/mg/tonymushah/itu/marary/entities/mono/medicaments/MethodeApplication.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import mg.tonymushah.itu.marary.entities.abstracts.EntityWithIDAndNom;
 
@@ -13,6 +14,7 @@ import mg.tonymushah.itu.marary.entities.abstracts.EntityWithIDAndNom;
 public class MethodeApplication extends EntityWithIDAndNom {
     @Id
     @Column(name = "id")
+    @SequenceGenerator(name = "method_appl_medicament_id_seq", sequenceName = "method_appl_medicament_id_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "method_appl_medicament_id_seq")
     private int id;
 
