9bf8c602b384ab7b15f72602f635df72ef0b785f
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 15:23:56 +0300

    Added `ContreIndication` entity

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/entities/complex/ContreIndication.java
@@ -0,0 +1,55 @@
+package mg.tonymushah.itu.marary.entities.complex;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.Optional;
+import mg.tonymushah.itu.marary.entities.abstracts.EntityWithID;
+import mg.tonymushah.itu.marary.repositories.complex.MedicamentRepository;
+
+@Entity
+@Table(name = "contre_indication")
+public class ContreIndication extends EntityWithID {
+    @Column(name = "medicament_1", nullable = false)
+    private int idMedicament1;
+    @Column(name = "medicament_2", nullable = false)
+    private int idMedicament2;
+
+    public int getIdMedicament1() {
+        return idMedicament1;
+    }
+
+    public void setIdMedicament1(int idMedicament1) {
+        this.idMedicament1 = idMedicament1;
+    }
+
+    public int getIdMedicament2() {
+        return idMedicament2;
+    }
+
+    public void setIdMedicament2(int idMedicament2) {
+        this.idMedicament2 = idMedicament2;
+    }
+
+    public ContreIndication() {
+    }
+
+    public ContreIndication(int idMedicament1, int idMedicament2) {
+        this.setIdMedicament1(idMedicament1);
+        this.setIdMedicament2(idMedicament2);
+    }
+
+    public ContreIndication(int id, int idMedicament1, int idMedicament2) {
+        super(id);
+        this.setIdMedicament1(idMedicament1);
+        this.setIdMedicament2(idMedicament2);
+    }
+
+    public Optional<Medicament> getMedicament1(MedicamentRepository repository) {
+        return repository.findById(this.getIdMedicament1());
+    }
+
+    public Optional<Medicament> getMedicament2(MedicamentRepository repository) {
+        return repository.findById(this.getIdMedicament2());
+    }
+}
