a866613f33da4976fbab5aa181371b7ccb4119cc
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 15:24:10 +0300

    Added `ContreIndicationRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/ContreIndicationRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.ContreIndication;
+
+public interface ContreIndicationRepository extends CrudRepository<ContreIndication, Integer> {
+
+}
