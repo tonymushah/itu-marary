1949a444e62331fca5974a844976545b05686ad2
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 15:50:57 +0300

    Added `PersonneAlergieRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/PersonneAlergieRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.PersonneAlergie;
+
+public interface PersonneAlergieRepository extends CrudRepository<PersonneAlergie, Integer> {
+
+}
