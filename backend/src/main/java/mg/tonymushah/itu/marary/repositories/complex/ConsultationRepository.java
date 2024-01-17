a84cd3b32f314e005ff9b9cd6003725b74b9f8b1
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 12:18:23 +0300

    Added `ConsultationRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/ConsultationRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.Consultation;
+
+public interface ConsultationRepository extends CrudRepository<Consultation, Integer> {
+
+}
