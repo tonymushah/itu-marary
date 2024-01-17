abb8d405a48b63bfa1d4943559ca38a0f0f2bca5
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 20:23:46 +0300

    Removed controllers

--- backend/src/main/java/mg/tonymushah/itu/marary/controllers/interfaces/CreateControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package mg.tonymushah.itu.marary.controllers.interfaces;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-public interface CreateControllerInterface<I, O> {
-    @PostMapping("/")
-    public O create(@RequestBody I input);
-}
--- backend/src/main/java/mg/tonymushah/itu/marary/controllers/interfaces/DeleteControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package mg.tonymushah.itu.marary.controllers.interfaces;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-public interface DeleteControllerInterface<I, O> {
-    @DeleteMapping("/{id}/")
-    public O delete(@PathVariable("id") I id);
-}
