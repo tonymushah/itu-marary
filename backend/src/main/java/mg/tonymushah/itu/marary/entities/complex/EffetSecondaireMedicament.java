903ce65bbc016b5f1a5b2f931a2281f6165cca62
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 16:04:17 +0300

    Added `EffetSecondaireMedicament`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/entities/complex/EffetSecondaireMedicament.java
@@ -0,0 +1,69 @@
+package mg.tonymushah.itu.marary.entities.complex;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.Optional;
+import mg.tonymushah.itu.marary.entities.abstracts.EntityWithID;
+import mg.tonymushah.itu.marary.entities.mono.Symptome;
+import mg.tonymushah.itu.marary.repositories.complex.MedicamentRepository;
+import mg.tonymushah.itu.marary.repositories.mono.SymptomeRepository;
+
+@Entity
+@Table(name = "effet_secondaire_medicament")
+public class EffetSecondaireMedicament extends EntityWithID {
+    @Column(name = "id_medicament", nullable = false)
+    private int idMedicament;
+    @Column(name = "id_symptome", nullable = false)
+    private int idSymptome;
+    @Column(name = "degree", nullable = false)
+    private int degree;
+
+    public int getIdMedicament() {
+        return idMedicament;
+    }
+
+    public void setIdMedicament(int idMedicament) {
+        this.idMedicament = idMedicament;
+    }
+
+    public int getIdSymptome() {
+        return idSymptome;
+    }
+
+    public void setIdSymptome(int idSymptome) {
+        this.idSymptome = idSymptome;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    public EffetSecondaireMedicament() {
+    }
+
+    public EffetSecondaireMedicament(int idMedicament, int idSymptome, int degree) {
+        this.setIdMedicament(idMedicament);
+        this.setIdSymptome(idSymptome);
+        this.setDegree(degree);
+    }
+
+    public EffetSecondaireMedicament(int id, int idMedicament, int idSymptome, int degree) {
+        super(id);
+        this.setIdMedicament(idMedicament);
+        this.setIdSymptome(idSymptome);
+        this.setDegree(degree);
+    }
+
+    public Optional<Medicament> getMedicament(MedicamentRepository repository) {
+        return repository.findById(this.getIdMedicament());
+    }
+
+    public Optional<Symptome> getSymptome(SymptomeRepository repository) {
+        return repository.findById(this.getIdSymptome());
+    }
+}
