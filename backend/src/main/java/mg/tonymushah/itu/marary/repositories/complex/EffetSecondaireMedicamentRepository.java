ef68c56b4bae24c16a8dc63704c95d61152f7a4c
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 16:05:53 +0300

    Added `EffetSecondaireMedicamentRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/EffetSecondaireMedicamentRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.EffetSecondaireMedicament;
+
+public interface EffetSecondaireMedicamentRepository extends CrudRepository<EffetSecondaireMedicament, Integer> {
+
+}
