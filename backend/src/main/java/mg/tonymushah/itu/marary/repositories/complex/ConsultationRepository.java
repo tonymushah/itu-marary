2927d447e2f26b3eff28c789aa54478f7a1154cc
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 13:11:29 +0300

    Added `ConsultationSymptomeRepository`

--- backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/ConsultationRepository.java
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/ConsultationRepository.java
@@ -1,9 +1,16 @@
 package mg.tonymushah.itu.marary.repositories.complex;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import mg.tonymushah.itu.marary.entities.complex.Consultation;
 
 public interface ConsultationRepository extends CrudRepository<Consultation, Integer> {
+    @Query(value = "SELECT * FROM consultation where id_personne = ?1", nativeQuery = true)
+    public Iterable<Consultation> findByPersonneId(int personneId);
 
+    @Query(value = "SELECT * FROM consultation where id_personne = ?1", countQuery = "SELECT count(*) FROM consultation where id_personne = ?1", nativeQuery = true)
+    public Page<Consultation> findByPersonneId(int personneId, Pageable pageable);
 }
---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/ConsultationSymptomeRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.ConsultationSymptome;
+
+public interface ConsultationSymptomeRepository extends CrudRepository<ConsultationSymptome, Integer> {
+
+}
