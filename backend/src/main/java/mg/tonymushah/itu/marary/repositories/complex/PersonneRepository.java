bc85fa232eb9ce37470c5705fb56c043b67f4ce3
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 12:13:19 +0300

    Added `PersonneRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/PersonneRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.Personne;
+
+public interface PersonneRepository extends CrudRepository<Personne, Integer> {
+    
+}
