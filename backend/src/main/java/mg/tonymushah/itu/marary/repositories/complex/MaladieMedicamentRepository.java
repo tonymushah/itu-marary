0a50ed9750a3abf0ccd6d5cc0eccce9c6fc6b6b8
tonymushah <tonyramanantsalama@gmail.com>
2024-01-17 15:08:13 +0300

    Added `MaladieMedicamentRepository`

---
+++ backend/src/main/java/mg/tonymushah/itu/marary/repositories/complex/MaladieMedicamentRepository.java
@@ -0,0 +1,9 @@
+package mg.tonymushah.itu.marary.repositories.complex;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.tonymushah.itu.marary.entities.complex.MaladieMedicament;
+
+public interface MaladieMedicamentRepository extends CrudRepository<MaladieMedicament, Integer> {
+
+}
